a64a28ac3c57ebf6a76dc9cf99336f051dc59f8e
AS <asadrgilany7@gmail.com>
2021-10-25 15:31:16 -0400

    added public files and finished fetch requests for the websites

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const { animals } = require("./data/animals.json");
 const fs = require("fs");
 const path = require("path");
+const { redirect } = require("statuses");
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static("public"));
 
 
 
@@ -101,6 +103,22 @@ app.post("/api/animals", (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
